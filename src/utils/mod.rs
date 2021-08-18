a5a134e624dba32806e0e35389c44853d253c96e
AndyIsCool5463 <andyl5463@gmail.com>
2021-08-17 21:28:29 -0700

    oops missed semicolon

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -7,3 +7,4 @@ pub fn read_config(file: &str) -> ConfigurationData {
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
     toml::from_str::<ConfigurationData>(&contents).unwrap()
+}
