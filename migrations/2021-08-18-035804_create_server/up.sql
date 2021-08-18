53ba122d5af59541187c706eca296ecca4eb8d90
AndyIsCool5463 <andyl5463@gmail.com>
2021-08-18 16:18:56 -0700

    remove diesel for sqlx

--- migrations/2021-08-18-035804_create_server/up.sql
+++
@@ -1,8 +0,0 @@
--- Your SQL goes here
-
-CREATE TABLE servers (
-    id SERIAL PRIMARY KEY,
-    guild BIGINT NOT NULL,
-    joined TIMESTAMPTZ NOT NULL,
-    blacklisted BOOLEAN NOT NULL DEFAULT 'f'
-)
--- src/schema.rs
+++
@@ -1,8 +0,0 @@
-table! {
-    servers (id) {
-        id -> Int4,
-        guild -> Int8,
-        joined -> Timestamptz,
-        blacklisted -> Bool,
-    }
-}
