fd05609daeee47ee007b18aed3ee03dede513a60
AndyIsCool5463 <andyl5463@gmail.com>
2021-08-17 22:20:55 -0700

    added data to the client container and ctrl + c handler

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
-use tracing::{error, info, instrument, Level};
-use crate::utils::{read_config, AppInfo};
 use serenity::framework::StandardFramework;
-use std::error::Error;
-use serenity::model::prelude::*;
-use dotenv::dotenv;
-use crate::config::ConfigurationData;
 use serenity::client::ClientBuilder;
 use serenity::client::bridge::gateway::GatewayIntents;
 use serenity::Client;
+use serenity::model::prelude::*;
 use reqwest::Client as ReqwestClient;
 use reqwest::redirect::Policy;
+use tracing::{error, info, instrument, Level};
+use dotenv::dotenv;
+use chrono::Utc;
+use std::sync::Arc;
+use std::error::Error;
+use crate::utils::{read_config, AppInfo};
+use crate::config::ConfigurationData;
+use crate::data::{ReqwestContainer, ConfigContainer, UptimeContainer, ShardManagerContainer};
+
 
 #[macro_use]
 extern crate diesel;
@@ -49,6 +53,20 @@ async fn main() {
         .intents(GatewayIntents::all())
         .framework(framework)
         .await?;
+
+    build_client_data(&client, &config).await;
+
+    let shard_manager = client.shard_manager.clone();
+
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await.expect("Could not register ctrl + c handler");
+        info!("Shutting down!");
+        shard_manager.lock().await.shutdown_all().await;
+    });
+
+    if let Err(why) = client.start_autosharded().await {
+        panic!("An error occurred while running the client: {:?}", why);
+    }
 }
 
 fn build_framework(app_info: AppInfo, config: ConfigurationData) -> StandardFramework {
@@ -65,12 +83,17 @@ fn build_framework(app_info: AppInfo, config: ConfigurationData) -> StandardFram
         })
 }
 
-async fn build_client_data(client: &Client) {
+async fn build_client_data(client: &Client, config: &ConfigurationData) {
     {
         // Lock so we can edit!
         let mut data = client.data.write().await;
 
         let http_client = ReqwestClient::builder().user_agent(constants::REQWEST_USER_AGENT).redirect(Policy::none()).build()?;
 
+
+        data.insert::<ReqwestContainer>(http_client);
+        data.insert::<ShardManagerContainer>(Arc::clone(&client.shard_manager));
+        data.insert::<ConfigContainer>(config.clone());
+        data.insert::<UptimeContainer>(Utc::now());
     }
 }
