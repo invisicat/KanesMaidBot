8e8aeb905ad7d26b9435122c46850a03314b620f
AndyIsCool5463 <andyl5463@gmail.com>
2021-08-17 21:21:39 -0700

    enable tokio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use tracing::{error, info, instrument, Level};
+use tracing_log::LogTracer;
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
+
+#[macro_use]
+extern crate diesel;
+
+
+pub mod schema;
+pub mod models;
+mod constants;
+
+
+#[tokio::main(worker_threads = 16)]
+#[instrument]
+async fn main() {
+    print!("\x1B[2J\x1B[1;1H");
+    println!("{}\n\n", constants::TITLE);
+
+
 }
